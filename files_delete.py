f85e341fae0ce5e1592e4f8777f2b144c375d410
FreyaAllan <142427624+FreyaAllan@users.noreply.github.com>
2023-09-17 00:43:16 +0100

    Update files_delete.py

--- files_delete.py
+++ files_delete.py
@@ -1,7 +1,7 @@
 import re
 import logging
 from pyrogram import Client, filters
-from info import DELETE_CHANNELS
+from info import DELETE_CHANNELS, Config
 from database.ia_filterdb import Media, unpack_new_file_id
 
 logger = logging.getLogger(__name__)
