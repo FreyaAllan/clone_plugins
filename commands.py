2cb2987e897d127ab150d446ee71302a3fa45f88
FreyaAllan <142427624+FreyaAllan@users.noreply.github.com>
2023-09-19 22:54:30 +0100

    Update commands.py

--- commands.py
+++ commands.py
@@ -275,6 +275,9 @@ async def start(client, message):
         await asyncio.sleep(300)
         await k.edit("<b>Your message is successfully deleted!!!</b>")
         return
+    except:
+        pass
+    return await message.reply('No such file exist. or no database added')
         
     
     elif data.startswith("short"):
@@ -296,6 +299,10 @@ async def start(client, message):
         await asyncio.sleep(1200)
         await k.edit("<b>Your message is successfully deleted!!!</b>")
         return
+    except:
+        pass
+    return await message.reply('No such file exist. or no database added')
+       
         
     elif data.startswith("all"):
         files = temp.GETALL.get(file_id)
@@ -351,7 +358,11 @@ async def start(client, message):
         for x in filesarr:
             await x.delete()
         await k.edit_text("<b>Your All Files/Videos is successfully deleted!!!</b>")
-        return    
+        return
+    except:
+        pass
+    return await message.reply('No such file exist. or no database added')
+           
         
     elif data.startswith("files"):
         user = message.from_user.id
@@ -377,6 +388,9 @@ async def start(client, message):
             await asyncio.sleep(1200)
             await k.edit("<b>Your message is successfully deleted!!!</b>")
             return
+        except:
+            pass
+        return await message.reply('No such file exist. or no database added')
     user = message.from_user.id
     files_ = await get_file_details(file_id)           
     if not files_:
@@ -430,7 +444,7 @@ async def start(client, message):
             return
         except:
             pass
-        return await message.reply('No such file exist.')
+        return await message.reply('No such file exist. or no database added')
     files = files_[0]
     title = f'{clonedme.B_NAME} ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
     size=get_size(files.file_size)
@@ -478,7 +492,11 @@ async def start(client, message):
     await asyncio.sleep(600)
     await msg.delete()
     await k.edit_text("<b>Your File/Video is successfully deleted!!!\n\nClick below button to get your deleted file 👇</b>",reply_markup=InlineKeyboardMarkup(btn))
-    return   
+    return
+    except:
+        pass
+    return await message.reply('No such file exist. or no database added')
+          
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
