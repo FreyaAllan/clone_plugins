971e30ccba098b307fa76080996e1e0e0c37a115
FreyaAllan <142427624+FreyaAllan@users.noreply.github.com>
2023-09-20 19:18:39 +0100

    Update channel.py

--- channel.py
+++ channel.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from info import CHANNELS, Config
-from database.ia_filterdb import save_file
+from clone_plugins.database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video | filters.audio
 
