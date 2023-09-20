d996eab72564f42e00620999f2a94c898c4ad8bd
FreyaAllan <142427624+FreyaAllan@users.noreply.github.com>
2023-09-20 21:50:37 +0100

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -62,7 +62,7 @@ async def save_file(media):
 
 
 
-async def get_search_results(query, file_type=None, max_results=(Config.MAX_RIST_BTNS), offset=0, filter=False):
+async def get_search_results(query, file_type=None, max_results=(Config.MAX_B_TN), offset=0, filter=False):
     """For given query return (results, next_offset)"""
 
     query = query.strip()
