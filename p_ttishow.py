0979e3436ff12b7019de58a9a6cbfd8f9f976a34
FreyaAllan <142427624+FreyaAllan@users.noreply.github.com>
2023-09-20 22:22:41 +0100

    Update p_ttishow.py

--- p_ttishow.py
+++ p_ttishow.py
@@ -162,18 +162,18 @@ async def re_enable_chat(bot, message):
     temp.BANNED_CHATS.remove(int(chat_))
     await message.reply("Chat Successfully re-enabled")
 
+@Client.on_message(filters.command('stats') & filters.incoming)
+async def get_ststs(bot, message):
+    rju = await message.reply('Fetching stats..')
+    total_users = await db.total_users_count()
+    totl_chats = await db.total_chat_count()
+    files = await Media.count_documents()
+    size = await db.get_db_size()
+    free = 536870912 - size
+    size = get_size(size)
+    free = get_size(free)
+    await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
 
-@Client.on_message(filters.command('stats'))
-def statsmsg(client, message):
-    buttons = [[
-                InlineKeyboardButton('STATUS', url=f'https://t.me/{temp.U_NAME}?start=status')
-            ]]
-    reply_markup=InlineKeyboardMarkup(buttons)
-    message.reply_photo(
-    photo=random.choice(PICS),
-    caption="click below 👇 button ✅ for STATUS",
-    reply_markup=reply_markup,
-    )    
 
 @Client.on_message(filters.command('invite') & filters.user(ADMINS))
 async def gen_invite(bot, message):
