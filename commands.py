a6e59c02b9f6120f6cc12afed10da649254910e4
FreyaAllan <142427624+FreyaAllan@users.noreply.github.com>
2023-09-26 18:56:27 +0100

    Update commands.py

--- commands.py
+++ commands.py
@@ -25,7 +25,7 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type == enums.ChatType.PRIVATE:
-        if not await db.is_user_found(message.from_user.id):
+        if not await db.is_user_exist(message.from_user.id):
             await db.add_user(message.from_user.id, message.from_user.first_name)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
 
