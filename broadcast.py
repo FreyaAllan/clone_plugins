4ba56407f324c31385daeaa579e2ff5353e883b9
FreyaAllan <142427624+FreyaAllan@users.noreply.github.com>
2023-10-07 22:38:31 +0100

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -1,12 +1,12 @@
-
 from pyrogram import Client, filters
 import datetime
 import time
 from clone_plugins.database.users_chats_db import db
-from info import ADMINS, Config
+from info import ADMINS
 from utils import broadcast_messages, broadcast_messages_group
 import asyncio
-        
+from cloneinfo import Config
+
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 # https://t.me/GetTGLink/4178
 async def verupikkals(bot, message):
