ac2e5cf5c1410862be46bcdbd0501daa7cb9a460
FreyaAllan <142427624+FreyaAllan@users.noreply.github.com>
2023-10-16 19:12:13 +0100

    Update cloneinfo.py

--- cloneinfo.py
+++ cloneinfo.py
@@ -13,15 +13,15 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 bots = list(mongo_db.bots.find())
-    for bot in bots:
-        bot_token = bot['token']
-        try:
-            ai = Client(
-                f"{bot_token}", API_ID, API_HASH,
-                bot_token=bot_token,
-                plugins={"root": "clone_plugins"},
-            )
-            bot = await ai.get_me()
+for bot in bots:
+bot_token = bot['token']
+    try:
+    ai = Client(
+        f"{bot_token}", API_ID, API_HASH,
+        bot_token=bot_token,
+        plugins={"root": "clone_plugins"},
+        )
+        bot = await ai.get_me()
 
 
 class Config:
